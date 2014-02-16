12d07442e795f05abcbb765538fa647d2d09f4d5
Dimitri Nicolas <dimitri.nicolas.cn@gmail.com>
2014-02-16 17:36:25 +0100

    Page 404 animé
    Pure css3 sans image \o/

--- 404.php
+++ 404.php
@@ -18,7 +18,21 @@
 			<?php include 'header.php'; ?>
 	
 			<section id="error404">
-				404
+
+				<div id="cloud">404</div>
+
+				
+
+				<ul id="rain">
+					<li></li><li></li><li></li><li></li><li></li><li></li><li></li><li></li><li></li><li></li>
+				</ul>
+
+				<div id="text">
+					<h1>Perdu sur DreamVids ?</h1>
+					Il semblerait que la page que vous cherchez n'existe pas. Vous pouvez faire une recherche ou retourner à l'<a href="/">accueil</a>.<br />
+					Si vous pensez que c'est un bug, informez-en l'équipe : <a href="mailto:bugreport@dreamvids.fr">bugreport@dreamvids.fr</a>
+				</div>
+
 			</section>
 
 			<?php include 'footer.php'; ?>
