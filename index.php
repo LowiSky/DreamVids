a4ea1d7075365e911b2c03f4f5dd1b682317e2c2
Dimitri Nicolas <dimitri.nicolas.cn@gmail.com>
2014-02-26 21:15:58 +0100

    Refonte total du css des miniatures de vidéos. Juste mis à jour sur la page d'accueil, je ferais les autres demain parce que là je vais regardé les derniers épisode de ma série du moment, y'a trop de suspense :O (BroadChurch)

--- index.php
+++ index.php
@@ -20,135 +20,130 @@
 			<div id="homeLargeModal">
 				<div id="backgroundLoader" class="bgLoader" data-background="img/fond.jpg"></div>
 				<section>
+					<?php if (!$connected) { ?>
+						<div id="boxPages" class="home">
+							<div id="pageHome">
+								<img src="img/icon_logo.png" alt="Logo DreamVids" class="logo"/>
+
+								<h3>Bienvenue sur DreamVids</h3>
+								<p class="inner-text">
+									Let us dream ! - Nouvelle plateforme ouverte, gratuite et conviviale de partage de contenu vidéo.
+								</p>
+
+								<button onclick="document.getElementById('boxPages').className = 'register';">S'inscrire</button>
+								<a onclick="document.getElementById('boxPages').className = 'login';">Se connecter</a>
+							</div>
+						
+							<div id="pageRegister">
+								<h3>S'inscrire</h3>
+
+								<form method="post" action="">
+									<label for="email">Adresse email :</label>
+									<input type="email" name="email" id="email" placeholder="Adresse email"/><br />
+									<label for="username">Pseudo :</label>
+									<input type="text" name="username" id="username" placeholder="Pseudo"/><br />
+									
+									<label for="pass">Mot de passe :</label>
+									<input type="password" name="pass" id="pass" placeholder="Mot de passe"/><br />
+									<label for="passConfirm">Confirmez le mot de passe :</label>
+									<input type="password" name="passConfirm" id="passConfirm" placeholder="Confirmation du mot de passe"/><br />
+									
+									<input type="checkbox" id="CGU" name="CGU"/><label for="CGU">J'accepte les conditions d'utilisations</label><br />
+									
+									<input type="submit" name="submit" value="S'inscrire"/>
+								</form>
+
+								<a onclick="document.getElementById('boxPages').className = 'login';">Se connecter</a>
+							</div>
+
+							<div id="pageLogin">
+								<h3>Se connecter</h3>
+
+								<form method="post" action="">
+									<label for="username">Pseudo :</label>
+									<input type="text" name="username" id="username" placeholder="Pseudo"/><br />
+									
+									<label for="pass">Mot de passe :</label>
+									<input type="password" name="pass" id="pass" placeholder="Mot de passe"/><br />
+									
+									<input type="submit" name="submit" value="Se connecter"/>
+								</form>
+
+								<a onclick="document.getElementById('boxPages').className = 'register';">S'inscrire</a>
+							</div>
+						</div>
+					<?php } else { ?>
+						<div id="boxPages" class="channel">
+							<div id="pageChannel">
+								<span class="avatar bgLoader" data-background="http://lorempicsum.com/simpsons/255/200/5"></span>
+								<h3>Ma chaine</h3>
+								<p class="inner-text">
+									Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quibusdam, cumque minus maiores debitis placeat !
+								</p>
+							</div>
+						</div>
+
+					<?php } ?>
+
+					<div id="boxBest">
+						<h3>Meilleures vidéos :</h3>
+						<ul id="sliderList" class="slide1">
+							<li onclick="slideTo(1);"></li>
+							<li onclick="slideTo(2);"></li>
+							<li onclick="slideTo(3);"></li>
+						</ul>
+
+						<section id="slider" class="slide1">
+
+							<div id="slide">
+								<div class="card video">
+									<div class="thumbnail bgLoader" style="height: 75%;" data-background="http://lorempicsum.com/rio/350/200/1"><a href="video" class="overlay"></a></div>
+									<div class="description">
+										<a href="video"><h4>Lorem Ipsum dolorem</h4></a>
+									</div>
+								</div>
+
+								<div class="card video">
+									<div class="thumbnail bgLoader" style="height: 75%;" data-background="http://lorempicsum.com/rio/627/300/4"><a href="video" class="overlay"></a></div>
+									<div class="description">
+										<a href="video"><h4>Rio !</h4></a>
+									</div>
+								</div>
+							</div>
+
+							<div id="slide">
+								<div class="card video">
+									<div class="thumbnail bgLoader" style="height: 75%;" data-background="http://lorempicsum.com/futurama/350/200/1"><a href="video" class="overlay"></a></div>
+									<div class="description">
+										<a href="video"><h4>Lorem Ipsum dolorem</h4></a>
+									</div>
+								</div>
 					
-					<?php 
-						if (!$connected) { ?>
-							<div id="boxPages" class="home">
-								<div id="pageHome">
-									<img src="img/icon_logo.png" alt="Logo DreamVids" class="logo"/>
-
-									<h3>Bienvenue sur DreamVids</h3>
-									<p class="inner-text">
-										Let us dream ! - Nouvelle plateforme ouverte, gratuite et conviviale de partage de contenu vidéo.
-									</p>
-
-									<button onclick="document.getElementById('boxPages').className = 'register';">S'inscrire</button>
-									<a onclick="document.getElementById('boxPages').className = 'login';">Se connecter</a>
-								</div>
-							
-								<div id="pageRegister">
-									<h3>S'inscrire</h3>
-
-									<form method="post" action="">
-										<label for="email">Adresse email :</label>
-										<input type="email" name="email" id="email" placeholder="Adresse email"/><br />
-										<label for="username">Pseudo :</label>
-										<input type="text" name="username" id="username" placeholder="Pseudo"/><br />
-										
-										<label for="pass">Mot de passe :</label>
-										<input type="password" name="pass" id="pass" placeholder="Mot de passe"/><br />
-										<label for="passConfirm">Confirmez le mot de passe :</label>
-										<input type="password" name="passConfirm" id="passConfirm" placeholder="Confirmation du mot de passe"/><br />
-										
-										<input type="checkbox" id="CGU" name="CGU"/><label for="CGU">J'accepte les conditions d'utilisations</label><br />
-										
-										<input type="submit" name="submit" value="S'inscrire"/>
-									</form>
-
-									<a onclick="document.getElementById('boxPages').className = 'login';">Se connecter</a>
-								</div>
-
-								<div id="pageLogin">
-									<h3>Se connecter</h3>
-
-									<form method="post" action="">
-										<label for="username">Pseudo :</label>
-										<input type="text" name="username" id="username" placeholder="Pseudo"/><br />
-										
-										<label for="pass">Mot de passe :</label>
-										<input type="password" name="pass" id="pass" placeholder="Mot de passe"/><br />
-										
-										<input type="submit" name="submit" value="Se connecter"/>
-									</form>
-
-									<a onclick="document.getElementById('boxPages').className = 'register';">S'inscrire</a>
+								<div class="card video">
+									<div class="thumbnail bgLoader" style="height: 75%;" data-background="http://lorempicsum.com/futurama/627/300/4"><a href="video" class="overlay"></a></div>
+									<div class="description">
+										<a href="video"><h4>Futurama !</h4></a>
+									</div>
 								</div>
 							</div>
-							<?php }
-								else { ?>
-								<div id="boxPages" class="channel">
-									<div id="pageChannel">
-										<span class="avatar bgLoader" data-background="http://lorempicsum.com/simpsons/255/200/5"></span>
-										<h3>Ma chaine</h3>
-										<p class="inner-text">
-											Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quibusdam, cumque minus maiores debitis placeat !
-										</p>
+
+							<div id="slide">
+								<div class="card video">
+									<div class="thumbnail bgLoader" style="height: 75%;" data-background="http://lorempicsum.com/simpsons/350/200/1"><a href="video" class="overlay"></a></div>
+									<div class="description">
+										<a href="video"><h4>Lorem Ipsum dolorem</h4></a>
 									</div>
 								</div>
 
-							<?php } ?>
-
-							<div id="boxBest">
-								<h3>Meilleures vidéos :</h3>
-								<ul id="sliderList" class="slide1">
-									<li onclick="slideTo(1);"></li>
-									<li onclick="slideTo(2);"></li>
-									<li onclick="slideTo(3);"></li>
-								</ul>
-
-								<section id="slider" class="slide1">
-
-									<div id="slide">
-										<div class="video">
-											<div class="video-thumbnail bgLoader" data-background="http://lorempicsum.com/rio/350/200/1">
-												<a href="video"><div class="video-overlay"><img src="img/play_icon_recomandations.png" alt="Regardez la vidéo 'Titre de la vidéo'"></div></a>
-											</div>
-											<div class="video-description"><a href="video"><h4>Lorem Ipsum dolorem</h4></a></div>
-										</div>
-							
-										<div class="video">
-											<div class="video-thumbnail bgLoader" data-background="http://lorempicsum.com/rio/627/300/4">
-												<a href="video"><div class="video-overlay"><img src="img/play_icon_recomandations.png" alt="Regardez la vidéo 'Titre de la vidéo'"></div></a>
-											</div>
-											<div class="video-description"><a href="video"><h4>Rio !</h4></a></div>
-										</div>
+								<div class="card video">
+									<div class="thumbnail bgLoader" style="height: 75%;" data-background="http://lorempicsum.com/simpsons/627/300/4"><a href="video" class="overlay"></a></div>
+									<div class="description">
+										<a href="video"><h4>The Simpsons !</h4></a>
 									</div>
-
-									<div id="slide">
-										<div class="video">
-											<div class="video-thumbnail bgLoader" data-background="http://lorempicsum.com/futurama/350/200/1">
-												<a href="video"><div class="video-overlay"><img src="img/play_icon_recomandations.png" alt="Regardez la vidéo 'Titre de la vidéo'"></div></a>
-											</div>
-											<div class="video-description"><a href="video"><h4>Lorem Ipsum dolorem</h4></a></div>
-										</div>
-							
-										<div class="video">
-											<div class="video-thumbnail bgLoader" data-background="http://lorempicsum.com/futurama/627/300/4">
-												<a href="video"><div class="video-overlay"><img src="img/play_icon_recomandations.png" alt="Regardez la vidéo 'Titre de la vidéo'"></div></a>
-											</div>
-											<div class="video-description"><a href="video"><h4>Futurama !</h4></a></div>
-										</div>
-									</div>
-
-									<div id="slide">
-										<div class="video">
-											<div class="video-thumbnail bgLoader" data-background="http://lorempicsum.com/simpsons/350/200/1">
-												<a href="video"><div class="video-overlay"><img src="img/play_icon_recomandations.png" alt="Regardez la vidéo 'Titre de la vidéo'"></div></a>
-											</div>
-											<div class="video-description"><a href="video"><h4>Lorem Ipsum dolorem</h4></a></div>
-										</div>
-							
-										<div class="video">
-											<div class="video-thumbnail bgLoader" data-background="http://lorempicsum.com/simpsons/627/300/4">
-												<a href="video"><div class="video-overlay"><img src="img/play_icon_recomandations.png" alt="Regardez la vidéo 'Titre de la vidéo'"></div></a>
-											</div>
-											<div class="video-description"><a href="video"><h4>The Simpsons !</h4></a></div>
-										</div>
-									</div>
-
-								</section>
+								</div>
 							</div>
-
+						</section>
+					</div>
 				</section>
 			</div>
 
@@ -207,85 +202,89 @@
 						<h3>Vidéos à découvrir</h3>
 					</div>
 					
-					<div class="video">
-						<div class="video-thumbnail bgLoader" data-background="http://lorempicsum.com/up/350/200/1">
-							<div class="video-time"><p>12:05</p></div>
-							<a href="video"><div class="video-overlay"><img src="img/play_icon_recomandations.png" alt="Regardez la vidéo 'Titre de la vidéo'"></div></a>
+					<div class="card video">
+						<div class="thumbnail bgLoader" data-background="http://lorempicsum.com/up/350/200/1">
+							<div class="time">12:05</div>
+							<a href="video" class="overlay"></a>
 						</div>
-						<div class="video-description">
+						<div class="description">
 							<a href="video"><h4>Up !</h4></a>
-							<div class="video-bottom-description">
-								<span class="video-view"><img src="img/view_icon_recomandation.png" alt="View of the video">12 530</span>
-								<span class="video-channel"><a href="#">Papy</a></span>
+							<div>
+								<span class="view">12 530</span>
+								<a class="channel" href="#">Papy</a>
 							</div>
 						</div>
 					</div>
-					<div class="video">
-						<div class="video-thumbnail bgLoader" data-background="http://lorempicsum.com/nemo/350/200/1">
-							<div class="video-time"><p>16:17</p></div>
-							<a href="video"><div class="video-overlay"><img src="img/play_icon_recomandations.png" alt="Regardez la vidéo 'Titre de la vidéo'"></div></a>
+
+					<div class="card video">
+						<div class="thumbnail bgLoader" data-background="http://lorempicsum.com/nemo/350/200/1">
+							<div class="time">16:17</div>
+							<a href="video" class="overlay"></a>
 						</div>
-						<div class="video-description">
+						<div class="description">
 							<a href="video"><h4>Nemo</h4></a>
-							<div class="video-bottom-description">
-								<span class="video-view"><img src="img/view_icon_recomandation.png" alt="View of the video">10 576</span>
-								<span class="video-channel"><a href="#">Dori</a></span>
+							<div>
+								<span class="view">10 576</span>
+								<a class="channel" href="#">Dori</a>
 							</div>
 						</div>
 					</div>
-					<div class="video">
-						<div class="video-thumbnail bgLoader" data-background="http://lorempicsum.com/simpsons/627/200/3">
-							<div class="video-time"><p>1:27:24</p></div>
-							<a href="video"><div class="video-overlay"><img src="img/play_icon_recomandations.png" alt="Regardez la vidéo 'Titre de la vidéo'"></div></a>
+
+					<div class="card video">
+						<div class="thumbnail bgLoader" data-background="http://lorempicsum.com/simpsons/627/200/3">
+							<div class="time">1:27:24</div>
+							<a href="video" class="overlay"></a>
 						</div>
-						<div class="video-description">
+						<div class="description">
 							<a href="video"><h4>Les Simpson, le film</h4></a>
-							<div class="video-bottom-description">
-								<span class="video-view"><img src="img/view_icon_recomandation.png" alt="View of the video">401</span>
-								<span class="video-channel"><a href="#">Home Simpson</a></span>
+							<div>
+								<span class="view">401</span>
+								<a class="channel" href="#">Home Simpson</a>
 							</div>
 						</div>
 					</div>
-					<div class="video">
-						<div class="video-thumbnail bgLoader" data-background="http://lorempicsum.com/nemo/627/300/4">
-							<div class="video-time"><p>3:27</p></div>
-							<a href="video"><div class="video-overlay"><img src="img/play_icon_recomandations.png" alt="Regardez la vidéo 'Titre de la vidéo'"></div></a>
+
+					<div class="card video">
+						<div class="thumbnail bgLoader" data-background="http://lorempicsum.com/nemo/627/300/4">
+							<div class="time">3:27</div>
+							<a href="video" class="overlay"></a>
 						</div>
-						<div class="video-description">
+						<div class="description">
 							<a href="video"><h4>Nemo [Bande Annonce]</h4></a>
-							<div class="video-bottom-description">
-								<span class="video-view"><img src="img/view_icon_recomandation.png" alt="View of the video">32 546</span>
-								<span class="video-channel"><a href="#">Nemo</a></span>
+							<div>
+								<span class="view">32 546</span>
+								<a class="channel" href="#">Nemo</a>
 							</div>
 						</div>
 					</div>
-					<div class="video">
-						<div class="video-thumbnail bgLoader" data-background="http://lorempicsum.com/rio/350/200/1">
-							<div class="video-time"><p>2:34:53</p></div>
-							<a href="video"><div class="video-overlay"><img src="img/play_icon_recomandations.png" alt="Regardez la vidéo 'Titre de la vidéo'"></div></a>
+
+					<div class="card video">
+						<div class="thumbnail bgLoader" data-background="http://lorempicsum.com/rio/350/200/1">
+							<div class="time">2:34:53</div>
+							<a href="video" class="overlay"></a>
 						</div>
-						<div class="video-description">
+						<div class="description">
 							<a href="video"><h4>Rio</h4></a>
-							<div class="video-bottom-description">
-								<span class="video-view"><img src="img/view_icon_recomandation.png" alt="View of the video">1 752</span>
-								<span class="video-channel"><a href="#">Hungry Bird</a></span>
+							<div>
+								<span class="view">1 752</span>
+								<a class="channel" href="#">Hungry Bird</a>
 							</div>
 						</div>
 					</div>
-					<div class="video">
-						<div class="video-thumbnail bgLoader" data-background="http://lorempicsum.com/up/627/300/4">
-							<div class="video-time"><p>2:43</p></div>
-							<a href="video"><div class="video-overlay"><img src="img/play_icon_recomandations.png" alt="Regardez la vidéo 'Titre de la vidéo'"></div></a>
+
+					<div class="card video">
+						<div class="thumbnail bgLoader" data-background="http://lorempicsum.com/up/627/300/4">
+							<div class="time">2:43</div>
+							<a href="video" class="overlay"></a>
 						</div>
-						<div class="video-description">
+						<div class="description">
 							<a href="video"><h4>La Haut ! Bande Annonce</h4></a>
-							<div class="video-bottom-description">
-								<span class="video-view"><img src="img/view_icon_recomandation.png" alt="View of the video">513</span>
-								<span class="video-channel"><a href="#">Pixar</a></span>
+							<div>
+								<span class="view">513</span>
+								<a class="channel" href="#">Pixar</a>
 							</div>
 						</div>
-					</div>
-	
+					</div>	
 				</aside>
 			</div>
 	
