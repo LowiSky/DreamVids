fd337e7b4310752b281a5ce6aa46c5dd3b85d3cd
Dimitri Nicolas <dimitri.nicolas.cn@gmail.com>
2014-03-21 18:35:36 +0100

    Footer trop swaggy

--- contributors.php
+++ contributors.php
@@ -26,14 +26,45 @@
 						<img class="avatar" src="http://phpeter.legtux.org/uploads/avatar.jpg">
 						<h1>Peter</h1>
 						<div class="description">19 ans, étudiant en L2 info, je me passionne pour l'automobile et l'informatique, je suis l'initiateur et co-fondateur de DreamVids. J'assure la gestion du projet, de l'équipe et une partie du code (programmation)</div>
-						<a class="button twitter" href="https://twitter.com/p_cauty">Twitter</a>
+						<a class="button twitter" target="_blank" href="https://twitter.com/p_cauty">Twitter</a>
 					</li>
 
 					<li>
 						<img class="avatar" src="https://pbs.twimg.com/profile_images/420266828368068609/7CHDztuB.png">
 						<h1>Stornitz</h1>
-						<div class="description">Jeune lycéen de 15 ans qui a pour passion l'informatique. Actuellement en 1ère S SI,il envisage par la suite des études d'ingénieur informatique, il a développé une API (https://github.com/JamesHemeryfr/DreamVids_API) et un lecteur externe (http://stornitz.fr/DreamVids/)</div>
-						<a class="button twitter" href="https://twitter.com/Stornitz">Twitter</a>
+						<div class="description">Jeune lycéen de 15 ans qui a pour passion l'informatique. Actuellement en 1ère S SI,il envisage par la suite des études d'ingénieur informatique, il a développé une API (<a href="https://github.com/JamesHemeryfr/DreamVids_API">github.com/JamesHemeryfr/DreamVids_API</a>) et un lecteur externe (<a href="http://stornitz.fr/DreamVids/">stornitz.fr/DreamVids</a>)</div>
+						<a class="button twitter" target="_blank" href="https://twitter.com/Stornitz">Twitter</a>
+					</li>
+
+					<li>
+						<img class="avatar" src="https://pbs.twimg.com/profile_images/378800000727360878/e9cd7f6cfbe5f600f8308ac057a38501.png">
+						<h1>Quadrifoglio</h1>
+						<div class="description">16 ans, 1&egrave;reS, passionn&eacute; d'informatique et d'astronomie depuis toujours. D&eacute;veloppeur serveur DreamVids, je travaille principalement de mani&egrave;re acharn&eacute;e sur la prochaine version du site !</p>
+						<a class="button twitter" target="_blank" href="https://twitter.com/_quadrifoglio">Twitter</a>
+					</li>
+					<li>
+						<img class="avatar" src="http://dreamvids.fr/uploads/Snapcube/avatar.png">
+						<h1>Snapcube</h1>
+						<div class="description">Snapcube, 15 ans, lyc&eacute;en en seconde. Adore l'informatique et surtout la programmation (surtout PHP et Java). Mod&eacute;rateur Dreamvids. A fait le Ctrl+Enter pour envoyer les commentaires :P. Suit le projet depuis le d&eacute;but avant d'avoir &eacute;t&eacute; recrut&eacute; en tant que mod&eacute;rateur.</p>
+						<a class="button twitter" target="_blank" href="https://twitter.com/TaccolaA">Twitter</a>
+					</li>
+					<li>
+						<img class="avatar" src="http://image.noelshack.com/fichiers/2014/09/1393266985-logo-bestt.png">
+						<h1>Bestt_Alex</h1>
+						<div class="description">Passionn&eacute; par l'informatique et plus particuli&eacute;rement par le domaine du web je suis quelqu'un de dynamique et toujours en qu&ecirc;te de savoir !</p>
+						<a class="button twitter" target="_blank" href="https://twitter.com/Bestt_Alex">Twitter</a>
+					</li>
+					<li>
+						<img class="avatar" src="https://pbs.twimg.com/profile_images/426859163226865664/k7LpkzoD.png">
+						<h1>James Hemery</h1>
+						<div class="description">Passionn&eacute; par les nouvelles technologies et le d&eacute;veloppement, j'aime apprendre de nouvelles notions et partager mon exp&eacute;rience en vid&eacute;o, j'ai r&eacute;alis&eacute; avec Stornitz l'API de DreamVids</p>
+						<a class="button twitter" target="_blank" href="https://twitter.com/JamesHemery">Twitter</a>
+					</li>
+					<li>
+						<img class="avatar" src="https://pbs.twimg.com/profile_images/417066062715158529/TB8tT9p0.png">
+						<h1>Jeremy</h1>
+						<div class="description">Jeremy, 15 ans, je g&eacute;re sur DreamVids le community management (Twitter/Email), je me passionne pour le d&eacute;veloppement web et les nouvelles technologies</p>
+						<a class="button twitter" target="_blank" href="https://twitter.com/jeremy__fr">Twitter</a>
 					</li>
 					
 				</ul>
--- footer.php
+++ footer.php
@@ -1,5 +1,25 @@
 <footer>
 	<div id="inner-footer">
+		<div class="row">
+			<h1>DreamVids</h1>
+			
+			<a href="contributors">Contributeurs</a>
+			<a href="http://dreamvids.net/">Blog de développement</a>
+		</div>
+
+		<div class="row">
+			<h1>Partenaires</h1>
+			
+			<a href="https://twitter.com/AdoptRecords">AdoptMusic</a>
+			<a href="#">Votre site ici ?</a>
+		</div>
+
+		<div class="row">
+			<h1>Social</h1>
+			
+			<a href="https://twitter.com/DreamVids_">Twitter</a>
+			<a href="https://facebook.com/DreamVids">Facebook</a>
+		</div>
 
 		<div class="rights">
 			<span class="love">Made with <i>♥</i> approximately in France</span>
